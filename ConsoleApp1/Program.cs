8e4b5ef9f3dbf6c071f3cb6006985b4278c8a517
nachomonllor <nachomonllorc@gmail.com>
2023-03-26 00:37:23 -0300

    isRotatedPalindrome método inicial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        public static bool isRotatedPalindrome(String s)
+        {
+            //"BAABCC"
+            // CBAABC
+
+            //for(int i = 0; i < s.Length; i++)
+            //{
+
+            //}
+            int i = 0;
+            int div = 0;
+
+            char actual = s[i];
+            int cont = 0;
+            while (i  < s.Length)
+            {
+                if(actual == s[i])
+                {
+                    cont++;
+                }
+                else
+                {
+                    if(cont > 1)
+                    {
+                        div = i;
+                        break;
+                    }
+
+                    cont = 0;
+                }
+                i++;
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
